577961f60a6e98d825a0fb1acf9811ade84de64c
Elizaveta-1207 <elizaveta1207@gmail.com>
2021-01-23 16:58:12 +0300

    feat: add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
+import GhIcon from '../../images/gh-icon.png';
+import FbIcon from '../../images/fb-icon.png';
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className='footer'>
+      <p className='footer__copyright'>&copy;2020 Supersite, Powered by News API</p>
+      <nav className='footer__nav'>
+        <div className='footer__links'>
+          <a href='/' className='footer__link'>
+            Главная
+          </a>
+          <a href='https://praktikum.yandex.ru/' className='footer__link'>
+            Яндекс Практикум
+          </a>
+        </div>
+        <div className='footer__socials'>
+          <a href='https://github.com/Elizaveta-1207' className='footer__social'>
+            <img src={GhIcon} className='footer__icon' alt='github link' />
+          </a>
+          <a href='https://www.facebook.com/' className='footer__social'>
+            <img src={FbIcon} className='footer__icon' alt='facebook link' />
+          </a>
+        </div>
+      </nav>
+    </footer>
+  );
 }
 
 export default Footer;
