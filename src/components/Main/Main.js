d74ca4e5be8ecc6e4a9a369f8555326ecc104505
Elizaveta-1207 <elizaveta1207@gmail.com>
2021-01-23 15:43:25 +0300

    feat: add main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import SearchForm from '../SearchForm/SearchForm';
 
 function Main() {
-  return <div>MAIN</div>;
+  return (
+    <div className='main'>
+      <h1 className='main__title'>Что творится в мире?</h1>
+      <p className='main__text'>
+        Находите самые свежие статьи на любую тему и сохраняйте в своём личном кабинете.
+      </p>
+      <SearchForm />
+    </div>
+  );
 }
 
 export default Main;
